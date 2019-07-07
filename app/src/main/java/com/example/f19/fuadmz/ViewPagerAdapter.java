4164f1fc91416f252dfe878100e22bf3a119842b
FuadMZ <zakiyyulfuad@gmail.com>
2019-07-07 15:19:27 +0700

    commit2

---
+++ app/src/main/java/com/example/f19/fuadmz/ViewPagerAdapter.java
@@ -0,0 +1,41 @@
+package com.example.f19.fuadmz;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+
+    private final List<Fragment> lstFragment = new ArrayList<>();
+    private final List<String> lstTitles = new ArrayList<String>();
+
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return lstFragment.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return lstTitles.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return (CharSequence) lstTitles.get(position);
+    }
+
+    public void AddFragment (Fragment fragment,String title){
+        lstFragment.add(fragment);
+        lstTitles.add(title);
+    }
+
+}
