707edf58a16959b859b454b894628b64e43ed555
FuadMZ <zakiyyulfuad@gmail.com>
2019-07-07 12:21:52 +0700

    commit1

---
+++ app/src/main/java/com/example/f19/fuadmz/SplashScreenActivity.java
@@ -0,0 +1,25 @@
+package com.example.f19.fuadmz;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private int waktu_loading = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splash = new Intent(SplashScreenActivity.this, WalktroughActivity.class);
+                startActivity(splash);
+                finish();
+            }
+        }, waktu_loading);
+    }
+}
